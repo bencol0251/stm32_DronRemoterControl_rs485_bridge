19822730e1d46b82aa89e565331e20eacfba25cc
KKN93 <kanclick0903@git.com>
2026-06-08 16:37:02 +0900

    Removed the <*.h> lines that were displayed in red.

--- STM32F103_MCP4131_Controller/Core/Inc/stm32f1xx_hal_conf.h
+++ STM32F103_MCP4131_Controller/Core/Inc/stm32f1xx_hal_conf.h
@@ -3,6 +3,7 @@
 
 #define HAL_MODULE_ENABLED
 #define HAL_GPIO_MODULE_ENABLED
+#define HAL_DMA_MODULE_ENABLED
 #define HAL_RCC_MODULE_ENABLED
 #define HAL_CORTEX_MODULE_ENABLED
 #define HAL_UART_MODULE_ENABLED
@@ -17,6 +18,7 @@
 #define HSI_VALUE              8000000U
 #define LSI_VALUE              40000U
 #define LSE_VALUE              32768U
+#define LSE_STARTUP_TIMEOUT    5000U
 #define VDD_VALUE              3300U
 #define TICK_INT_PRIORITY      0U
 #define USE_RTOS               0U
@@ -24,10 +26,18 @@
 
 #include "stm32f1xx_hal_rcc.h"
 #include "stm32f1xx_hal_gpio.h"
+#include "stm32f1xx_hal_dma.h"
 #include "stm32f1xx_hal_cortex.h"
 #include "stm32f1xx_hal_uart.h"
 #include "stm32f1xx_hal_spi.h"
 #include "stm32f1xx_hal_tim.h"
 #include "stm32f1xx_hal_flash.h"
+
+#ifdef USE_FULL_ASSERT
+void assert_failed(uint8_t *file, uint32_t line);
+#define assert_param(expr) ((expr) ? (void)0U : assert_failed((uint8_t *)__FILE__, __LINE__))
+#else
+#define assert_param(expr) ((void)0U)
+#endif /* USE_FULL_ASSERT */
 
 #endif
